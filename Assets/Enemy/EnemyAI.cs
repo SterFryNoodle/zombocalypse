3bb3db0ef00792a298dcb10eb8b69d3fbd8866ca
SterFryNoodle <revangcruz@yahoo.com>
2024-07-19 23:51:49 -0700

    Enemy will now chase player indefinitely after entering chase range; Will attack once enemy reaches player

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -10,6 +10,7 @@ public class EnemyAI : MonoBehaviour
 
     NavMeshAgent agent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     void Start()
     {
@@ -25,15 +26,42 @@ public class EnemyAI : MonoBehaviour
     {
         distanceToTarget = Vector3.Distance(enemyTarget.position, transform.position); //Set variable to distance between player & enemy.
 
-        if (distanceToTarget < chaseRange)
+        if (isProvoked)
         {
-            agent.SetDestination(enemyTarget.position);
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;            
         }
     }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, chaseRange);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);        
+    }
+
+    void EngageTarget()
+    {        
+        if (distanceToTarget >= agent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= agent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    void ChaseTarget()
+    {
+        agent.SetDestination(enemyTarget.position);
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log("Enemy has attacked.");
     }
 }
