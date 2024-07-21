56ffef21bd706dba5f61e8869251d07945acb8f1
SterFryNoodle <revangcruz@yahoo.com>
2024-07-20 23:25:46 -0700

    Create a weapon script & have it do something when mouse button is pressed

---
+++ Assets/Player/Weapon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{      
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //do something.
+        }
+    }
+}
