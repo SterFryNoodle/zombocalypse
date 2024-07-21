9589f7a7e98615490a43859b16a6aaa66f01f118
SterFryNoodle <revangcruz@yahoo.com>
2024-07-21 15:55:29 -0700

    Added to weapon script to send raycast to tell when player clicks on a collider

--- Assets/Player/Weapon.cs
+++ Assets/Player/Weapon.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
-{      
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float maxDistance = 100f;
+    
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetButtonDown("Fire1"))
         {
-            //do something.
+            Shoot();
         }
     }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, maxDistance);
+
+        Debug.Log("Hit" + hit.transform.name);
+    }
+
+    
 }
