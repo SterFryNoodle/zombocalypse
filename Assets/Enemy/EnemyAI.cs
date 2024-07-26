e7fbf1ad917786b08497978aa26926e84df8e59d
SterFryNoodle <revangcruz@yahoo.com>
2024-07-26 00:02:23 -0700

    Enemy rotates to player while attacking instead of attacking in one direction

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform enemyTarget;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float rotationSpeed = 2f;
 
     NavMeshAgent agent;
     float distanceToTarget = Mathf.Infinity;
@@ -26,7 +27,7 @@ public class EnemyAI : MonoBehaviour
     {
         distanceToTarget = Vector3.Distance(enemyTarget.position, transform.position); //Set variable to distance between player & enemy.
         
-        if (isProvoked)
+        if (isProvoked) //Once provoked, call function.
         {            
             EngageTarget();
         }
@@ -36,15 +37,17 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    void OnDrawGizmosSelected()
+    void OnDrawGizmosSelected() //Creates visual representation of enemy range.
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, chaseRange);        
     }
 
     void EngageTarget()
-    {        
-        if (distanceToTarget > agent.stoppingDistance)
+    {
+        FaceTarget();
+
+        if (distanceToTarget > agent.stoppingDistance) 
         {
             ChaseTarget();
         }
@@ -57,14 +60,21 @@ public class EnemyAI : MonoBehaviour
 
     void ChaseTarget()
     {        
-        GetComponent<Animator>().SetBool("Attack", false);
+        GetComponent<Animator>().SetBool("Attack", false); //Set animation transition states for chasing.
         GetComponent<Animator>().SetTrigger("Walk");
-        agent.SetDestination(enemyTarget.position);
+        agent.SetDestination(enemyTarget.position); // Set navmesh agent to it's target position.
     }
 
     void AttackTarget()
     {
-        GetComponent<Animator>().SetBool("Attack", true);
-        Debug.Log("Enemy has attacked.");
+        GetComponent<Animator>().SetBool("Attack", true); //Set animation states for attacking.        
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (enemyTarget.position - transform.position).normalized; //Set vector's magnitude between the enemy & target to be normalized.
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z)); //Sets variable to be the new rotation based on direction vector.
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed); //Sets enemy rotation to interpolate from current rotation transform
+                                                                                                                 //to the new lookRotation @ a set speed.
     }
 }
