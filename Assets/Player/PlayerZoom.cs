ce650bf8d4b279b3c699629a7c39a4e99b5e4b18
SterFryNoodle <revangcruz@yahoo.com>
2024-08-01 22:44:02 -0700

    Decreased mouse sensivity when zoomed in

--- Assets/Player/PlayerZoom.cs
+++ Assets/Player/PlayerZoom.cs
@@ -2,30 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using StarterAssets;
 
 public class PlayerZoom : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera cameraZoom;
+    [SerializeField] FirstPersonController mouseSensitivity;
     [SerializeField] float zoomIn = 20f;
     [SerializeField] float zoomOut = 60f;
+    [SerializeField] float increaseSensitivity = 2f;
+    [SerializeField] float decreaseSensitivity = 0.5f;
 
-    bool zoomInToggle = false;
+    bool zoomInToggle = false;    
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) // decreases FOV & rotation speed.
         {
             if (zoomInToggle == false)
             {
                 zoomInToggle = true;
                 cameraZoom.m_Lens.FieldOfView = zoomIn;
+                mouseSensitivity.RotationSpeed = decreaseSensitivity;
             }            
         }
         else if (Input.GetMouseButtonUp(1))
         {        
-            if (zoomInToggle == true)
+            if (zoomInToggle == true) // defaults FOV & player rotation speed.
             {
                 zoomInToggle = false;
                 cameraZoom.m_Lens.FieldOfView = zoomOut;
+                mouseSensitivity.RotationSpeed = increaseSensitivity;
             }            
         }
     }
