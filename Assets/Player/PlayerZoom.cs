252c6827c22642cb83ee0ab8d0816d68aa35eb17
SterFryNoodle <revangcruz@yahoo.com>
2024-08-01 22:19:26 -0700

    Added a player zoom on RMB

--- Assets/Player/PlayerZoom.cs
+++ Assets/Player/PlayerZoom.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerZoom : MonoBehaviour
 {
-    [SerializeField] Camera cameraZoom;
+    [SerializeField] CinemachineVirtualCamera cameraZoom;
     [SerializeField] float zoomIn = 20f;
     [SerializeField] float zoomOut = 60f;
+
+    bool zoomInToggle = false;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomInToggle == false)
+            {
+                zoomInToggle = true;
+                cameraZoom.m_Lens.FieldOfView = zoomIn;
+            }            
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {        
+            if (zoomInToggle == true)
+            {
+                zoomInToggle = false;
+                cameraZoom.m_Lens.FieldOfView = zoomOut;
+            }            
+        }
+    }
 }
