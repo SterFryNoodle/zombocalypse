01798ea7f881a49cd8640d77980f3b5b8e6613a0
SterFryNoodle <revangcruz@yahoo.com>
2024-08-01 20:57:21 -0700

    Adjustments to navmesh; added zoom script; Adjustments to FOV

--- Assets/Player/PlayerZoom.cs
+++ Assets/Player/PlayerZoom.cs
@@ -3,18 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerZoom : MonoBehaviour
-{    
-    [SerializeField] float zoomAmount;
-    [SerializeField] float zoomIn = 40f;
-    [SerializeField] float zoomOut = 40f;
-
-    void Start()
-    {        
-        zoomAmount = GetComponent<Camera>().fieldOfView;
-    }
-    
-    void Update()
-    {
-        
-    }
+{
+    [SerializeField] Camera cameraZoom;
+    [SerializeField] float zoomIn = 20f;
+    [SerializeField] float zoomOut = 60f;
 }
