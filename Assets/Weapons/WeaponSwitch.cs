9967ddef49702215fcfec5a012808fb80d24eb93
SterFryNoodle <revangcruz@yahoo.com>
2024-08-06 22:32:40 -0700

    Switches weapon active based on their index under the weapon object parent

--- Assets/Weapons/WeaponSwitch.cs
+++ Assets/Weapons/WeaponSwitch.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int currentWeapon = 0;
+    [SerializeField] List<WeaponSwitch> weapons;
+
     void Start()
+    {
+        SetWeaponActive();
+    }
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetWeaponActive()
     {
-        
+        int weaponIndex = 0;
+
+        foreach (Transform weapon in transform) //Finds the transform of each child object from object script is attached to.
+        {
+            if (weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            weaponIndex++; //Adds counter to check index of each weapon child until it finds the right one.
+        }
     }
 }
