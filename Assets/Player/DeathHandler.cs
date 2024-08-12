a6cfae82f300f7076f81429d9f048423b55766b2
SterFryNoodle <revangcruz@yahoo.com>
2024-08-11 20:24:19 -0700

    fixed bug of being able to still switch weapons after death

--- Assets/Player/DeathHandler.cs
+++ Assets/Player/DeathHandler.cs
@@ -15,6 +15,8 @@ public class DeathHandler : MonoBehaviour
     {
         gameOverScreen.enabled = true;
         Time.timeScale = 0; //Stops time in game.
+        FindObjectOfType<WeaponSwitch>().enabled = false; //Disables the WeaponSwitch script after game is paused.
+
         Cursor.lockState = CursorLockMode.None; //Unlocks cursor from center of screen.
         Cursor.visible = true; //Makes cursor visible to player.
     }
