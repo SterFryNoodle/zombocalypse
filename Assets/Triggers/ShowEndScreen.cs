884ec3358ed05454b01117afb11548d72f8745b6
SterFryNoodle <revangcruz@yahoo.com>
2024-09-17 19:30:10 -0700

    Created an end game screen

--- Assets/Triggers/ShowEndScreen.cs
+++ Assets/Triggers/ShowEndScreen.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarterAssets;
 
 public class ShowEndScreen : MonoBehaviour
 {
-    
-    void Start()
+    [SerializeField] Canvas endOfGameScreen;
+
+    private void Start()
     {
-        
+        endOfGameScreen.enabled = false;
     }
 
-    
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        endOfGameScreen.enabled = true;
+        Time.timeScale = 0; //Stops time in game.
+
+        FindObjectOfType<WeaponSwitch>().enabled = false; //Disables the WeaponSwitch script after game is paused.
+        FindObjectOfType<FirstPersonController>().enabled = false; //Disables player from looking around.
+        FindObjectOfType<PlayerZoom>().enabled = false; //Disables player from zooming in.
+
+        Cursor.lockState = CursorLockMode.None; //Unlocks cursor from center of screen.
+        Cursor.visible = true; //Makes cursor visible to player.
     }
 }
