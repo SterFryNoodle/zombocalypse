ca73a6db3788f35eaafb987ab683398f56529f57
SterFryNoodle <revangcruz@yahoo.com>
2024-09-19 19:53:57 -0700

    Added audio files; Added player animator; Added scripts to play sfx during animations

--- Assets/Triggers/SpawnZombie.cs
+++ Assets/Triggers/SpawnZombie.cs
@@ -6,16 +6,42 @@ public class SpawnZombie : MonoBehaviour
 {
     [SerializeField] GameObject enemyObject;
     [SerializeField] int spawnTimer = 1;
+    [SerializeField] GameObject gameManager;
+    [SerializeField] float lowerVolume = .1f;
+    [SerializeField] AudioClip suspenseAudio;
+
+    AudioSource audioSource;
+    bool isStartingSequence = false;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(isStartingSequence)
+        {
+            TransitionBackgroundMusic();
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && enemyObject != null)
         {
+            isStartingSequence = true;
+            audioSource.clip = suspenseAudio;
+            audioSource.Play();
             enemyObject.SetActive(true);
             StartCoroutine(RunIntoDistance());
         }
     }
-    
+
+    void TransitionBackgroundMusic()
+    {
+        gameManager.GetComponent<AudioSource>().volume -= lowerVolume * Time.deltaTime;        
+    }
+
     IEnumerator RunIntoDistance()
     {
         yield return new WaitForSeconds(spawnTimer);
