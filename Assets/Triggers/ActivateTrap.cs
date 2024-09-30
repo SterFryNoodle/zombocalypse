bc0464c507bcdad260e31965a986e8a59776dfae
SterFryNoodle <revangcruz@yahoo.com>
2024-09-30 11:10:58 -0700

    added animation & sound fx when trapdoors are triggered

--- Assets/Triggers/ActivateTrap.cs
+++ Assets/Triggers/ActivateTrap.cs
@@ -7,10 +7,33 @@ public class ActivateTrap : MonoBehaviour
 {
     [SerializeField] Transform trapDoor;
     [SerializeField] int trapDoorDelay = 3;
-            
-    void OnTriggerEnter()
+    [SerializeField] AudioClip trapDoorSound;
+    [SerializeField] float travelDistance = 14f;
+    [SerializeField] float speed = 2f;
+
+    AudioSource audioSource;
+    bool hasEntered = false;
+    Vector3 direction = new Vector3(0,0,-1);
+    Vector3 startPosition;
+    Vector3 endPosition;
+    void Start()
     {
-        StartCoroutine(DelayTrap());
+        audioSource = GetComponent<AudioSource>();
+        startPosition = trapDoor.position;
+        endPosition = startPosition + direction.normalized * travelDistance;
+    }
+
+    void Update()
+    {
+        if(hasEntered)
+        {
+            trapDoor.position = Vector3.MoveTowards(trapDoor.position, endPosition, speed * Time.deltaTime);
+        }        
+    }
+
+    void OnTriggerEnter()
+    {        
+        StartCoroutine(DelayTrap());        
     }
 
     IEnumerator DelayTrap()
@@ -20,6 +43,8 @@ public class ActivateTrap : MonoBehaviour
         if (trapDoor != null)
         {
             trapDoor.gameObject.SetActive(true);
+            audioSource.PlayOneShot(trapDoorSound);
+            hasEntered = true;
         }
     }
 }
